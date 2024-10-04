ecbf71fe2c0cefa55edea0edba82a75262179e00
SuperBadgerBoy <kylerwee@outlook.com>
2024-10-04 08:20:42 +0000

    no change

--- main.py
+++ main.py
@@ -12,11 +12,8 @@ def CheckVistorRobot(name):
         print("what are we going to learn today?")     
     else:
         print("shoo!get away before I take my poison-bullet gun and fight U of!!!!!!!!!!!!!!!!!!!!!!")
-#CheckVistorRobot("alfred")
-def Xtimescheater(num2,num7):
-    ans = num2*num7
-    print(ans)
-Xtimescheater(12345286294648395829405862835962456829486029484384956,14728349274567284983748237438453732839458234736248284762578)
+CheckVistorRobot("")
+
     
     
 
