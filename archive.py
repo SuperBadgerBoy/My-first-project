877849e98457e71a609fe65fbab037d5f60db7d8
SuperBadgerBoy <kylerwee@outlook.com>
2024-10-04 07:53:55 +0000

    4 oct 24

--- archive.py
+++ archive.py
@@ -1,4 +1,7 @@
 
+num = int(input("what is the times table of a number you want to know"))
+for kyler in range(1,28):
+    print (str(kyler) + "x" + str(num) + "=" + str(num*kyler))
 
 
 
--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-num = int(input("what is the times table of a number you want to know"))
-for kyler in range(1,28):
-    print (str(kyler) + "x" + str(num) + "=" + str(num*kyler))
+def CheckVistorRobot(name):
+    name = input ("hello ,what wis you r cuteq naM4ED?")
+    if name == "kyler" or name == "spotty" or name =="badger" or name =="tig":
+        print("welcome home, " + name)
+    elif name =="jamie" or name=="jing tian":
+        print("Hallo, nice to meet U!")
+    elif name == "I steaks":
+        print("get out of here, horrid " + name )
+    elif name =="christina" or name =="kendrick":
+        print("hallo and welcome.How's your work?")
+    elif name =="alfred":
+        print("what are we going to learn today?")     
+    else:
+        print("shoo!get away before I take my poison-bullet gun and fight U of!!!!!!!!!!!!!!!!!!!!!!")
+#CheckVistorRobot("alfred")
+def Xtimescheater(num2,num7):
+    ans = num2*num7
+    print(ans)
+Xtimescheater(12345286294648395829405862835962456829486029484384956,14728349274567284983748237438453732839458234736248284762578)
+    
+    
+
+
+
